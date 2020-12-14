714756ec46be11c875889b8fedbd6df830a36471
segitiga <rafi.nizar15@gmail.com>
2020-12-14 14:43:50 +0700

    added: white layout

---
+++ resources/views/main/navbars/_navbar-white.blade.php
@@ -0,0 +1,49 @@
+<nav id="navwhite" class="navbar fixed-top navbar-expand-lg fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="/"><span id="jd1">Kita</span><span id="jd2">Kursus</span><span id="jd3">Kuy!</span></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Kursus
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="#">Kategori 1</a>
+                        <a class="dropdown-item" href="#">Kategori 2</a>
+                        <a class="dropdown-item" href="#">Kategori 3</a>
+                        <a class="dropdown-item" href="#">Kategori 4</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        Privat
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                        <a class="dropdown-item" href="#">Kategori 1</a>
+                        <a class="dropdown-item" href="#">Kategori 2</a>
+                        <a class="dropdown-item" href="#">Kategori 3</a>
+                        <a class="dropdown-item" href="#">Kategori 4</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPages" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        {{-- <img src="default-profile.jpg" alt=""> --}}
+                        User
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPages">
+                        <a class="dropdown-item" href="#">Dashboard (mentor only)</a>
+                        <a class="dropdown-item" href="#">Pembelian Saya (user only)</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
