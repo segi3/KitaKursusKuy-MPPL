9e4bf7edf9abeb9ba6a687234ae4ba42b6d53086
segitiga <rafi.nizar15@gmail.com>
2020-12-16 11:25:19 +0700

    updated: login user login mentor
    ganti warna button primary

--- resources/views/main/pages/mentor-login.blade.php
+++ resources/views/main/pages/mentor-login.blade.php
@@ -3,40 +3,44 @@
 @section('title', 'Login Mentor')
 
 @section('stylesheets')
-  <style>
-  .login {
-      margin-top: 40px;
-  }
+<style>
+    .login {
+        margin-top: 40px;
+    }
 
-  .child-div {
-      height: 100%
-  }
+    .child-div {
+        height: 100%
+    }
+    #login-area {
+        height: 79vh;
+    }
 
-  </style>
+</style>
 
 @endsection
 
 @section('content')
-<div class="container pb-5 pt-5">
-    <div class="row login">
+<div class="container py-5 mt-5" id="login-area">
+    <div class="row login child-div mt-5 pt-5">
         <div class="col-lg-6">
-            <img class="card-img" src="http://127.0.0.1:8000/main_resources/imgs/jumbotron1.jpeg" alt="">
+            <img class="card-img" src="http://127.0.0.1:8000/main_resources/imgs/jumbotron1.jpeg" alt="" height="270px">
         </div>
         <div class="col-lg-3 parent-div">
             <div class="row child-div">
                 <div class="col-12">
                     <h3 id="login">Login Mentor</h3>
                     <form>
-                    <div class="form-group">   
-                        <input type="email" class="form-control" placeholder="Enter Email" name="email" required> 
-                    </div>
-                    <div class="form-group">  
-                        <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
-                    </div>
-                    <div class="form-group">  
-                    Belum punya akun? <a href="#"> daftar disini </a>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button> 
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Enter Email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Enter Password" name="password"
+                                required>
+                        </div>
+                        <div class="form-group">
+                            Belum punya akun? <a href="#"> daftar disini </a>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
                     </form>
                 </div>
 
--- resources/views/main/pages/user-login.blade.php
+++ resources/views/main/pages/user-login.blade.php
@@ -3,22 +3,26 @@
 @section('title', 'Login')
 
 @section('stylesheets')
-  <style>
-  .login {
-      margin-top: 40px;
-  }
+<style>
+    .login {
+        margin-top: 40px;
+    }
 
-  .child-div {
-      height: 100%
-  }
+    .child-div {
+        height: 100%
+    }
 
-  </style>
+    #login-area {
+        height: 79vh;
+    }
+
+</style>
 
 @endsection
 
 @section('content')
-<div class="container pb-5 pt-5">
-    <div class="row login">
+<div class="container py-5 mt-5" id="login-area">
+    <div class="row login mt-5 pt-5">
         <div class="col-lg-6">
             <img class="card-img" src="http://127.0.0.1:8000/main_resources/imgs/jumbotron1.jpeg" alt="">
         </div>
@@ -27,19 +31,19 @@
                 <div class="col-12">
                     <h3 id="login">Login</h3>
                     <form>
-                    <div class="form-group">   
-                        <input type="email" class="form-control" placeholder="Enter Email" name="email" required> 
-                    </div>
-                    <div class="form-group">  
-                        <input type="password" class="form-control" placeholder="Enter Password" name="password" required>
-                    </div>
-                    <div class="form-group">  
-                    Belum punya akun? <a href="#"> daftar disini </a>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button> 
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="Enter Email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Enter Password" name="password"
+                                required>
+                        </div>
+                        <div class="form-group">
+                            Belum punya akun? <a href="#"> daftar disini </a>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
