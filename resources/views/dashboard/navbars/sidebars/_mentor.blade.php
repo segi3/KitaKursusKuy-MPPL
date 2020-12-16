23a6429e691f786e3db3f2251548bac4450b461e
segitiga <rafi.nizar15@gmail.com>
2020-12-16 11:02:46 +0700

    updated: sidebar topbar dashboard

--- resources/views/dashboard/navbars/sidebars/_mentor.blade.php
+++ resources/views/dashboard/navbars/sidebars/_mentor.blade.php
@@ -2,10 +2,10 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
+        {{-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        </div> --}}
+        <div class="sidebar-brand-text mx-3"><span id="jd1">Kita</span><span id="jd2">Kursus</span><span id="jd3">Kuy!</span></div>
     </a>
 
     <!-- Divider -->
@@ -15,92 +15,75 @@
     <li class="nav-item active">
         <a class="nav-link" href="index.html">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <span>Home</span></a>
     </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
+    <!-- Heading Kursus -->
     <div class="sidebar-heading">
-        Interface
+        Kursus
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item - Kursus -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseKursus" aria-expanded="true"
+            aria-controls="collapseKursus">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Kursus Saya</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseKursus" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+                <h6 class="collapse-header">Kursus</h6>
+                <a class="collapse-item" href="buttons.html">Kursus saya</a>
+
+                <h6 class="collapse-header">Murid</h6>
+                <a class="collapse-item" href="buttons.html">Lihat murid</a>
             </div>
         </div>
     </li>
+    
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-            aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-            aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
+    <!-- Nav Item - Kursus -->
     <li class="nav-item">
         <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Buat Kursus</span></a>
     </li>
 
-    <!-- Nav Item - Tables -->
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading Privat -->
+    <div class="sidebar-heading">
+        Privat
+    </div>
+
+    <!-- Nav Item - Privat -->
     <li class="nav-item">
-        <a class="nav-link" href="tables.html">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePrivat" aria-expanded="true"
+            aria-controls="collapsePrivat">
             <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
+            <span>Privat Saya</span>
+        </a>
+        <div id="collapsePrivat" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Privat</h6>
+                <a class="collapse-item" href="buttons.html">Privat Saya</a>
+
+                <h6 class="collapse-header">Murid</h6>
+                <a class="collapse-item" href="cards.html">Murid saya</a>
+            </div>
+        </div>
+    </li>
+    
+
+    <!-- Nav Item - Privat -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Buat Privat</span></a>
     </li>
 
     <!-- Divider -->
