9f91bdb02a3b44806a86b4ebfd51ea25e13a37d7
rafi nizar <rafi.nizar15@gmail.com>
2020-12-23 21:52:00 +0700

    updated: tambah form search

--- resources/views/main/navbars/_navbar-white.blade.php
+++ resources/views/main/navbars/_navbar-white.blade.php
@@ -7,6 +7,10 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-primary my-2 my-sm-0" type="submit">Cari</button>
+            </form>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown"
