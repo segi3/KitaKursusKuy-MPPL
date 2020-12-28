35553e5ffaef81171e4f5b869ffb3fb8502b1939
rafi nizar <rafi.nizar15@gmail.com>
2020-12-28 20:51:30 +0700

    added: tambah halaman

--- app/Http/Controllers/DashboardPageController.php
+++ app/Http/Controllers/DashboardPageController.php
@@ -20,4 +20,24 @@ class DashboardPageController extends Controller
     {
         return view('dashboard.pages.kursus-buat');
     }
+
+    function getPrivatSaya()
+    {
+        return view('dashboard.pages.privat-my');
+    }
+
+    function getBuatPrivat()
+    {
+        return view('dashboard.pages.privat-buat');
+    }
+
+    function getMuridPrivat()
+    {
+        return view('dashboard.pages.murid-privat');
+    }
+
+    function getPermintaanPrivat()
+    {
+        return view('dashboard.pages.permintaan-privat');
+    }
 }
